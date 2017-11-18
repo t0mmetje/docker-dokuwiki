591449c3333aca228ea4b41e8f0869edbbdc9193
Tom <tom@azmei.org>
2017-11-18 14:14:53 +0100

    Create users.auth.php
    The user credentials for authenticating to DokuWiki are: "superuser" with the password "password"

---
+++ users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+superuser:$1$AOuGP4Gx$lFtA1fjsY.0IfrdMbUYln.:real-name:email@address.tld:admin,user
